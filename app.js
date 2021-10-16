ca7210786cd96553779c9f9de1da19da8b380826
NyronFlackters <cruzsuazocristianandres@gmail.com>
2021-10-16 10:39:30 -0500

    Actualizar
    Actualizar

--- app.js
+++ app.js
@@ -6,25 +6,17 @@ const app = express()
 const port = 5500
 
 app.use(express.json({limit: "50mb"}))
-app.use(cors({origin:"*"}))
+app.use(cors())
 app.post("/auth/sign-in",(req,res) => {
-
-
-    if(req.body ){
         console.log(req.body)
         res.status(200),
         res.send({"token":"logueado"})
-}/*else{
-        console.log('Credenciales incorrectas')
-        res.status(404),
-        res.send()
-    }*/
 }
 )
 
 app.put("/light",(req,res) =>{
-    console.log(req.body.power)
-    res.status(204);
+    console.log(req.body)
+    res.send();
 })
 
     app.listen(port, ()=> {
